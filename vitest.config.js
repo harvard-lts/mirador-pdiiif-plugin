9c4ac88ecb32842ff6473059266803fa017638d8
rsa684 <rebecca_asch@harvard.edu>
2026-01-15 10:12:03 -0500

    switch to vite

---
+++ vitest.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  root: 'demo',
+  server: {
+    port: 9000,
+    open: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
